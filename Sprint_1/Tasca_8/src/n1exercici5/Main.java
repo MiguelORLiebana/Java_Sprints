5246078a040244b0954506f28994ce908d40bcbb
Miguel_Ortiz <miguelliebana83@gmail.com>
2024-03-14 18:37:23 +0100

    tasca_7 n1ex5 functional interface

--- Sprint_1/Tasca_8/src/n1exercici5/Main.java
+++ Sprint_1/Tasca_8/src/n1exercici5/Main.java
@@ -1,5 +1,29 @@
 package n1exercici5;
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        //Forma 1
+        FunctInterface obj = new FunctInterface() {
+            @Override
+            public double getValue() {
+                return 3.1415;
+            }
+        };
+
+        System.out.printf(String.valueOf(obj.getValue()));
+
+
+        //Forma 2
+        //https://www.geeksforgeeks.org/functional-interfaces-java/   - @FunctionalInterface Annotation
+
+//        FunctInterface obj1 = () -> 3.1;
+//
+//        System.out.printf(String.valueOf(obj1.getValue()));
+    }
+}
+
+@FunctionalInterface
+interface FunctInterface {
+   double getValue();
 }
