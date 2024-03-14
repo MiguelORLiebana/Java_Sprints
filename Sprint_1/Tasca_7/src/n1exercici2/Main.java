0e13496cffeeb8c5e3d463607cd7a2c05f664f49
Mike <mike@gmail.com>
2024-03-14 10:47:29 +0100

    tasca7 n1ex2 update warning annotation

--- Sprint_1/Tasca_7/src/n1exercici2/Main.java
+++ Sprint_1/Tasca_7/src/n1exercici2/Main.java
@@ -2,10 +2,8 @@ package n1exercici2;
 
 
 public class Main {
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings({"removal"})
     public static void main(String[] args) {
-
-        //https://www.tutorialspoint.com/the-suppresswarnings-annotation-in-java#:~:text=The%20%40SuppressWarnings%20annotation%20belongs%20to,and%20the%20warning%20is%20unwarranted.
 
         Treballador treballador = new Treballador("Marc", "Pou", 20);
         TreballadorPresencial treballadorP = new TreballadorPresencial("Lluisa", "Perez", 30);
