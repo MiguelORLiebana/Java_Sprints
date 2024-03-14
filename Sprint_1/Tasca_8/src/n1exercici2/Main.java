f8027d0080993113b0c382bc75d0f82d82584455
Mike <mike@gmail.com>
2024-03-14 13:38:02 +0100

    tasca 7 n1ex2 cadena que contienen letra y logitud

--- Sprint_1/Tasca_8/src/n1exercici2/Main.java
+++ Sprint_1/Tasca_8/src/n1exercici2/Main.java
@@ -1,5 +1,40 @@
 package n1exercici2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        List<String> llista = new ArrayList<String>();
+        llista.add("lluvia");
+        llista.add("julia");
+        llista.add("coche");
+        llista.add("nieve");
+        llista.add("pon");
+        llista.add("faraon");
+
+        List<String> llistaFinal;
+
+        llistaFinal = retornarLlistaO(llista);
+
+        mostrarLlista(llistaFinal);
+    }
+
+    public static List<String> retornarLlistaO(List<String> llista){
+
+        List<String> aux;
+
+//        for (String a : llista) { if (a.contains("o")) aux.add(a); }
+
+        aux = llista.stream().filter(a -> a.contains("o")).filter(a -> a.length() > 5).collect(Collectors.toList());
+
+        return aux;
+    }
+
+    public static void mostrarLlista(List<String> llista){
+
+        llista.forEach(System.out::println);
+    }
 }
