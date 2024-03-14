4ffbd9aa21ca3cf779925f86e875937d908cf7c6
Miguel_Ortiz <miguelliebana83@gmail.com>
2024-03-14 16:43:32 +0100

    tasca_7 n1ex4 reference method

--- Sprint_1/Tasca_8/src/n1exercici4/Main.java
+++ Sprint_1/Tasca_8/src/n1exercici4/Main.java
@@ -1,5 +1,21 @@
 package n1exercici4;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        List<String> meses = cargarMeses();
+
+        //method reference
+        meses.forEach(System.out::println);
+    }
+
+    public static List<String> cargarMeses(){
+        List<String> aux = Arrays.asList("gener","febrer","març","abril","maig","juny","juliol","agost","setembre","octubre","novembre","desembre");
+
+        return aux;
+    }
 }
