dfec31618ffc343edcd5224ca8b464e44c01a50b
Mike <mike@gmail.com>
2024-03-14 13:44:58 +0100

    tasca 7 n1ex3 mostrar meses

--- Sprint_1/Tasca_8/src/n1exercici3/Main.java
+++ Sprint_1/Tasca_8/src/n1exercici3/Main.java
@@ -1,5 +1,34 @@
 package n1exercici3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+     List<String> meses = cargarMeses();
+
+     meses.forEach(a -> System.out.println(a));
+
+    }
+
+    public static List<String> cargarMeses(){
+        List<String> aux = new ArrayList<>();
+        aux.add("gener");
+        aux.add("febrer");
+        aux.add("març");
+        aux.add("abril");
+        aux.add("maig");
+        aux.add("juny");
+        aux.add("juliol");
+        aux.add("agost");
+        aux.add("setembre");
+        aux.add("agost");
+        aux.add("setembre");
+        aux.add("octubre");
+        aux.add("novembre");
+        aux.add("desembre");
+
+        return aux;
+    }
 }
