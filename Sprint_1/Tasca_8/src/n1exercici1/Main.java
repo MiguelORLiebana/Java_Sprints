75f5b77973be08ceea11af64da2c284ddb2df831
Mike <mike@gmail.com>
2024-03-14 13:34:44 +0100

    tasca 7 n1ex1 cadena que contienen letra

--- Sprint_1/Tasca_8/src/n1exercici1/Main.java
+++ Sprint_1/Tasca_8/src/n1exercici1/Main.java
@@ -1,7 +1,38 @@
 package n1exercici1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world");
+
+        List<String> llista = new ArrayList<String>();
+        llista.add("lluvia");
+        llista.add("julia");
+        llista.add("coche");
+        llista.add("nieve");
+
+        List<String> llistaFinal;
+
+        llistaFinal = retornarLlistaO(llista);
+
+        mostrarLlista(llistaFinal);
+    }
+
+    public static List<String> retornarLlistaO(List<String> llista){
+
+        List<String> aux;
+
+//        for (String a : llista) { if (a.contains("o")) aux.add(a); }
+
+        aux = llista.stream().filter(a -> a.contains("o")).collect(Collectors.toList());
+
+        return aux;
+    }
+
+    public static void mostrarLlista(List<String> llista){
+
+        llista.forEach(System.out::println);
     }
 }
