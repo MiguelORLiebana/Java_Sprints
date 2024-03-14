791ae849e16b68734bf550e045669398ca4ca94f
Mike <mike@gmail.com>
2024-03-14 12:23:53 +0100

    tasca4 n1ex4 Main implementation

--- Sprint_1/Tasca_5/src/n1exercici4/Main.java
+++ Sprint_1/Tasca_5/src/n1exercici4/Main.java
@@ -1,5 +1,20 @@
 package n1exercici4;
 
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        String dir = System.getProperty("user.dir");
+        System.out.println(dir);
+        //String directory = dir +File.separator+File.separator+"Tasca_5"+File.separator+"src"+File.separator+"n1exercici4"+File.separator+"Readme.md";
+
+        Llista llista = new Llista();
+
+        try{
+            llista.mostrarConsolaFitxer(args[0]);
+
+        } catch(ArrayIndexOutOfBoundsException e){
+            e.getMessage();
+        }
+    }
 }
