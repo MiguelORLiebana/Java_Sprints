f6dea9f19c52f9f6258f69e1afcfe0fd0487ea03
Miguel_Ortiz <miguelliebana83@gmail.com>
2024-03-15 15:16:14 +0100

    tasca_8 n1ex8 String reverse

--- Sprint_1/Tasca_8/src/n1exercici8/Main.java
+++ Sprint_1/Tasca_8/src/n1exercici8/Main.java
@@ -1,5 +1,23 @@
 package n1exercici8;
 
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+        String name = "hola";
+
+
+        IReverse reverseF = (a) -> {
+            StringBuffer str = new StringBuffer(a);
+            str.reverse();
+            return str.toString();
+        };
+
+        System.out.printf(reverseF.reverse(name));
+
+    }
+}
+
+@FunctionalInterface
+interface IReverse{
+    String reverse(String reverse);
 }
