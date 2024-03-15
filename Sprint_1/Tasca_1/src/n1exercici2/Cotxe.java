2b6e3bfe416c88165363cf88c22640edfe9619e0
Miguel_Ortiz <miguelliebana83@gmail.com>
2024-03-15 15:52:16 +0100

    tasca_1 n1ex2 diferecias entre static, final, non static

--- Sprint_1/Tasca_1/src/n1exercici2/Cotxe.java
+++ Sprint_1/Tasca_1/src/n1exercici2/Cotxe.java
@@ -1,11 +1,12 @@
 package n1exercici2;
 
 public class Cotxe {
-    private static final String marca = "Opel";
-    private static String model;
-    private final int potencia;
+    public static final String marca = "Opel";
+    public static String model;
+    public final int potencia;
 
     public Cotxe(String model, int potencia){
+
         //inicialització al constructor dels atributs static i final
         this.model= model;
         this.potencia = potencia;
--- Sprint_1/Tasca_1/src/n1exercici2/Main.java
+++ Sprint_1/Tasca_1/src/n1exercici2/Main.java
@@ -3,11 +3,17 @@ package n1exercici2;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Invocar mètode estàtic:");
+        System.out.println("Invocar mètode estàtic FRENAR:");
         Cotxe.frenar();
 
-        System.out.println("\nInvocar métode no estàtic:");
+        System.out.println("\nInvocar métode no estàtic ACCELERAR:");
         Cotxe cotxe = new Cotxe("Corsa", 100);
         cotxe.accelerar();
+
+        System.out.println("***** variable static - final - static final *****");
+        System.out.println("Variable static model: " + Cotxe.model);
+        System.out.println("Variable static final: " + Cotxe.marca);
+        System.out.println("Variable final: " + cotxe.potencia);
+
     }
 }
