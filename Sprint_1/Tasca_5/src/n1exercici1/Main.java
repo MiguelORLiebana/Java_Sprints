d412d754a85c27ac8f1bdb9705ab07bc17e06dc5
Miguel_Ortiz <miguelliebana83@gmail.com>
2024-03-16 18:25:07 +0100

    tasca_5 n1ex1 dictory string redefined

--- Sprint_1/Tasca_5/src/n1exercici1/Main.java
+++ Sprint_1/Tasca_5/src/n1exercici1/Main.java
@@ -1,17 +1,19 @@
 package n1exercici1;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
 
-        //String directory = "/Users/mike/Desktop";
+        //String directory = "C:"+ File.separator+"Users"+File.separator+"hp"+File.separator+"Desktop"+File.separator+"git_JavaSprint"+File.separator+"Java_Sprints";
+        //System.out.printf("directory");
 
         Llista llista = new Llista();
 
         try{
             llista.llistarContingut(args[0]);
         } catch(ArrayIndexOutOfBoundsException e){
-            e.getMessage();
+            System.out.printf(e.getMessage());
         }
     }
-
 }
