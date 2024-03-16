5b136408e43b4483c2606746977efb5cd76a793f
Miguel_Ortiz <miguelliebana83@gmail.com>
2024-03-16 18:30:08 +0100

    tasca_5 n1ex1 new info added in readme.txt

--- Sprint_1/Tasca_5/src/n1exercici1/Main.java
+++ Sprint_1/Tasca_5/src/n1exercici1/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
 
         //String directory = "C:"+ File.separator+"Users"+File.separator+"hp"+File.separator+"Desktop"+File.separator+"git_JavaSprint"+File.separator+"Java_Sprints";
-        //System.out.printf("directory");
 
         Llista llista = new Llista();
 
