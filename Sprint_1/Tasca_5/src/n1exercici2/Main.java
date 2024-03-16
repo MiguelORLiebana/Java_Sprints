ca83b6d1466e041e5e275b14c2eaf24bb4ba8acd
Miguel_Ortiz <miguelliebana83@gmail.com>
2024-03-16 18:40:36 +0100

    tasca_5 n1ex2 new info added in readme.txt

--- Sprint_1/Tasca_5/src/n1exercici2/Main.java
+++ Sprint_1/Tasca_5/src/n1exercici2/Main.java
@@ -1,9 +1,12 @@
 package n1exercici2;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
 
-        String directory = "/Users/mike/Desktop/Test";
+        //String directory = "C:"+ File.separator+"Users"+File.separator+"hp"+File.separator+"Desktop"+File.separator+"git_JavaSprint"+File.separator+"Java_Sprints";
+
         Llista directoris = new Llista();
 
         try{
