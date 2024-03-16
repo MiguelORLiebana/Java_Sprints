6efe5a5b0d91b412545e02ee397d01ced5f85156
Miguel_Ortiz <miguelliebana83@gmail.com>
2024-03-16 18:15:53 +0100

    tasca_4 n1ex3 unnecessary imports deleted

--- Sprint_1/Tasca_4/src/n1exercici3/ExceptionTest.java
+++ Sprint_1/Tasca_4/src/n1exercici3/ExceptionTest.java
@@ -1,18 +1,14 @@
 package n1exercici3;
 
 import junit.framework.TestCase;
-import n1exercici1.Month;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class ExceptionTest extends TestCase {
     @Test
     void CathExceptionTest(){
+
         ExceptionCustom aux = new ExceptionCustom();
         Exception exception = assertThrows(ArrayIndexOutOfBoundsException.class, () -> aux.getException());
 
