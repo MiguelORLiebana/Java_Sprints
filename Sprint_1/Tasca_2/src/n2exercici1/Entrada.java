d14df44e482fcd874a16b50f3870cbebc8221f1a
Miguel <miguelliebana83@gmail.com>
2024-03-20 00:01:54 +0100

    InProgress: tasca_2 n2ex1 manage the exceptions

--- Sprint_1/Tasca_2/src/n2exercici1/Entrada.java
+++ Sprint_1/Tasca_2/src/n2exercici1/Entrada.java
@@ -1,4 +1,152 @@
 package n2exercici1;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Entrada {
+
+    static Scanner scanner = new Scanner(System.in);
+    public void executar() throws InterruptedException {
+        Byte valorByte = llegirByte("Introdueix la teva edat: ");
+        Integer valorInteger = llegirInt("Introdueix una xifra de tipus Integer");
+        Float valorDecimal = llegirFloat("Introdueix un nombre decimal: ");
+        Double valorDouble = llegirDouble("Introdueix un nombre double: ");
+        char valorChar = llegirChar("Introdueix un char: ");
+        String valorString = llegirString("Introdueix una paraula o text: ");
+        Boolean valorBoolean = llegirSiNo("Introdueix s o n: ");
+    }
+
+    //*************** InputMismatchException ********************
+    static byte llegirByte(String missatge){
+        Boolean notOk = false;
+        Byte nextByte;
+
+        while(!notOk){
+            System.out.println(missatge);
+            try{
+                nextByte = scanner.nextByte();
+                notOk = true;
+                return nextByte;
+            } catch(InputMismatchException e){
+                System.out.println("Error: edat no es correcte!");
+                scanner.nextLine();      // Limpieza de buffer -> https://es.stackoverflow.com/questions/357304/bucle-infinito-con-scanner-de-java
+            }
+        }
+        return -0;
+    }
+
+    public static int llegirInt(String missatge) {
+        Boolean notOk = false;
+        Integer nextInt;
+
+        while(!notOk){
+            System.out.println(missatge);
+            try{
+                nextInt = scanner.nextInt();
+                notOk = true;
+                return nextInt;
+            } catch(InputMismatchException e){
+                System.out.println("Error: la xifra no es correcte!");
+                scanner.nextLine();      // Limpieza de buffer -> https://es.stackoverflow.com/questions/357304/bucle-infinito-con-scanner-de-java
+            }
+        }
+        return -0;
+    }
+
+    public static float llegirFloat(String missatge) {
+        Boolean notOk = false;
+        Float nextFloat;
+
+        while(!notOk){
+            System.out.println(missatge);
+            try{
+                nextFloat = scanner.nextFloat();
+                notOk = true;
+                return nextFloat;
+            } catch(InputMismatchException e){
+                System.out.println("Error: nombre no es correcte!");
+                scanner.nextLine();      // Limpieza de buffer -> https://es.stackoverflow.com/questions/357304/bucle-infinito-con-scanner-de-java
+            }
+        }
+        return -0;
+    }
+
+    public static double llegirDouble(String missatge) {
+        Boolean notOk = false;
+        Double nextDouble;
+
+        while(!notOk){
+            System.out.println(missatge);
+            try{
+                nextDouble = scanner.nextDouble();
+                notOk = true;
+                return nextDouble;
+            } catch(InputMismatchException e){
+                System.out.println("Error: No és correcte, NO és un double!");
+                scanner.nextLine();      // Limpieza de buffer -> https://es.stackoverflow.com/questions/357304/bucle-infinito-con-scanner-de-java
+            }
+        }
+        return -0;
+    }
+
+    //*************** Exception ********************
+
+    public static char llegirChar(String missatge) {
+        Boolean notOk = false;
+        char nextChar;
+
+        while(!notOk){
+            System.out.println(missatge);
+            try{
+                nextChar = scanner.next().charAt(0);
+                notOk = true;
+                return nextChar;
+            } catch(Exception e){
+                System.out.println("Error: No és correcte, NO és un char!");
+                scanner.nextLine();      // Limpieza de buffer -> https://es.stackoverflow.com/questions/357304/bucle-infinito-con-scanner-de-java
+            }
+        }
+        return -0;
+    }
+
+    public static String llegirString(String missatge) {
+        Boolean notOk = false;
+        String nextString;
+
+        while(!notOk){
+            System.out.println(missatge);
+            try{
+                nextString = scanner.nextLine();
+                notOk = true;
+                return nextString;
+            } catch(Exception e){
+                System.out.println("Error: No és correcte, NO és un char!");
+                scanner.nextLine();      // Limpieza de buffer -> https://es.stackoverflow.com/questions/357304/bucle-infinito-con-scanner-de-java
+            }
+        }
+        return "";
+    }
+
+    public static boolean llegirSiNo(String missatge){
+        Boolean notOk = false;
+        String nextLine;
+
+        while(!notOk){
+            System.out.println(missatge);
+            try{
+                nextLine = scanner.nextLine();
+
+                if(nextLine.equals("s") || nextLine.equals("n")) {
+                    notOk = true;
+                    return true;
+                }
+            } catch(Exception e){
+                System.out.println("Error: No és correcte, NO és un char!");
+                scanner.nextLine();      // Limpieza de buffer -> https://es.stackoverflow.com/questions/357304/bucle-infinito-con-scanner-de-java
+            }
+        }
+        return false;
+    }
 }
+
+
--- Sprint_1/Tasca_2/src/n2exercici1/Main.java
+++ Sprint_1/Tasca_2/src/n2exercici1/Main.java
@@ -7,57 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Integer h = 8;
-        System.out.printf(String.valueOf(h.getClass()));
-        Scanner scanner = new Scanner(System.in);
+        Entrada entrada = new Entrada();
 
-//        Integer edad = scanner.nextInt();
-//        String dni = scanner.nextLine();d
-    }
-
-    //*************** InputMismatchException ********************
-    public static byte llegirByte(String missatge){
-        //TODO
-        byte a = 1;
-        return a;
-    }
-
-    public static int llegirInt(String missatge) {
-        //TODO
-        Integer a = 1;
-        return a;
-    }
-
-    public static float llegirFloat(String missatge) {
-        //TODO
-        float a = 1;
-        return a;
-    }
-
-    public static double llegirDouble(String missatge) {
-        //TODO
-        double a = 1;
-        return a;
-    }
-
-    //*************** Exception ********************
-
-    public static char llegirChar(String missatge) {
-        //TODO
-        char a = 'a';
-        return a;
-    }
-
-    public static String llegirString(String missatge) {
-        //TODO
-        String a = "";
-        return a;
-    }
-
-    public static boolean llegirSiNo(String missatge){
-        //si l’usuari/ària introdueix “s”, retorna “true”, si l’usuari introdueix “n”, retorna “false”.
-        //TODO
-        Boolean a = false;
-        return a;
+        try{
+            entrada.executar();
+        }catch(Exception e){
+            System.out.printf("Error");
+        }
     }
 }
