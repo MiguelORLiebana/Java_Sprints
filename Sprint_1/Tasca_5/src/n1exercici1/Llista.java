8794ac5260569c86b6b7c4d82678d0dd8e5ee393
Miguel <miguelliebana83@gmail.com>
2024-03-20 17:39:39 +0100

    Tasca5 n2ex1 files directory sorted

--- Sprint_1/Tasca_5/src/n1exercici1/Llista.java
+++ Sprint_1/Tasca_5/src/n1exercici1/Llista.java
@@ -2,13 +2,15 @@ package n1exercici1;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Llista {
 
     public void llistarContingut(String directory) {
 
         File file = new File(directory);
-        String[] aux = file.list();
+        List<String> aux = Arrays.asList(file.list());
 
         System.out.println("************* Llista sense ordre alfabètic **********");
         for(String a: aux){
@@ -16,9 +18,12 @@ public class Llista {
         }
 
         System.out.println("************* Llista per ordre alfabètic ***********");
-        Arrays.sort(aux);
+        List<String> llistaOrdre = aux
+                .stream()
+                .sorted()
+                .collect(Collectors.toList());
 
-        for(String a: aux){
+        for(String a: llistaOrdre){
             System.out.println(a);
         }
     }
--- Sprint_1/Tasca_5/src/n1exercici1/Main.java
+++ Sprint_1/Tasca_5/src/n1exercici1/Main.java
@@ -1,16 +1,14 @@
 package n1exercici1;
 
-import java.io.File;
-
 public class Main {
     public static void main(String[] args) {
 
         //String directory = "C:"+ File.separator+"Users"+File.separator+"hp"+File.separator+"Desktop"+File.separator+"git_JavaSprint"+File.separator+"Java_Sprints";
-
+        String directory = System.getProperty("user.dir");
         Llista llista = new Llista();
 
         try{
-            llista.llistarContingut(args[0]);
+            llista.llistarContingut(directory);
         } catch(ArrayIndexOutOfBoundsException e){
             System.out.printf(e.getMessage());
         }
