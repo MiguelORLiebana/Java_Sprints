6db66c1097427cf9fc487062d983998803d28f04
Miguel_Ortiz <miguelliebana83@gmail.com>
2024-03-26 16:02:52 +0100

    tasca1 n2ex1 feeback comments

--- Sprint_1/Tasca_1/src/n2exercici1/Smartphone.java
+++ Sprint_1/Tasca_1/src/n2exercici1/Smartphone.java
@@ -7,10 +7,13 @@ public class Smartphone extends Telefon implements Camera, Rellotge  {
         this.marca = marca;
         this.model = model;
     }
+
+    @Override
     public void fotografiar(){
         System.out.println("S’està fent una foto");
     }
 
+    @Override
     public void alarma(){
         System.out.println("Està sonant l’alarma");
     }
