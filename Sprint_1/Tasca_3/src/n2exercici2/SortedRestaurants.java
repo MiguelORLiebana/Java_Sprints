006f8f55ae85f67cf25112e0ccaf4e88c387fa14
Miguel_Ortiz <miguelliebana83@gmail.com>
2024-03-27 11:17:12 +0100

    tasca3 n2ex2 Sorted Restaurants

--- Sprint_1/Tasca_3/src/n2exercici2/SortedRestaurants.java
+++ Sprint_1/Tasca_3/src/n2exercici2/SortedRestaurants.java
@@ -2,7 +2,7 @@ package n2exercici2;
 
 import java.util.Comparator;
 
-class SortedRestaurants implements Comparator<n2exercici2.Restaurant> {
+class SortedRestaurants implements Comparator<Restaurant> {
     @Override
     public int compare(Restaurant a, Restaurant b) {
         int i = a.getName().compareTo(b.getName());
